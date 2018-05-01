30425f7b2fddd95c91acc5ed57d29f4817de51ef
shiwei <2651395747@qq.com>
2018-05-01 19:23:26 +0800

    OO.java

---
+++ OO.java
@@ -0,0 +1,17 @@
+package gitTest.gitskills;
+
+import java.util.List;
+
+public class OO {
+	public static void main(String[] args) {
+           System.out.println(1);
+	}
+	
+	public String method1(){
+		System.out.println(1);
+	}
+	
+	public String method2(){
+		System.out.println(2);
+	}
+}
